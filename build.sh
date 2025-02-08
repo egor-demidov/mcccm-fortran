99f91daf642c9fb8b70cf93065bb6161e6b3f90b
Egor Demidov <mail@edemidov.com>
2025-02-08 17:56:39 -0500

    Backup

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ cd build
 gfortran -c ../geometry.f90
 gfortran -c ../main.f90
 # Compile the C++ sources
-g++ -c ../geometry.cpp -o geometry_cxx.o
+g++ -c ../geometry.cpp -o geometry_cxx.o    # custom object file name to prevent name overlaps
 g++ -c ../generated_geometry.cpp
 g++ -c ../geometry_c_binding.cpp
 # Link the object files into an executable (-lgfortran is required to link the Fortran standard library)
