f39314d9da279cd03dd05a8dd7ff8b88c6606aa9
Egor Demidov <mail@edemidov.com>
2025-02-08 17:55:50 -0500

    Added a build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Create a build directory for an out-of-tree build
+mkdir -p build
+cd build
+
+# Compile the Fortran sources
+gfortran -c ../geometry.f90
+gfortran -c ../main.f90
+# Compile the C++ sources
+g++ -c ../geometry.cpp -o geometry_cxx.o
+g++ -c ../generated_geometry.cpp
+g++ -c ../geometry_c_binding.cpp
+# Link the object files into an executable (-lgfortran is required to link the Fortran standard library)
+g++ *.o -lgfortran -o main
+
+# Return to source directory
+cd ..
